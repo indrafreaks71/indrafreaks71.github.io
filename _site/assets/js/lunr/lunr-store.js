0e9c190ca161e7f5f74f26c37304f97756d1a01d
indrafreaks71 <167006071@student.unsil.ac.id>
2018-04-07 10:41:46 -0400

    First Commit

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "My first post",
+        "excerpt":"my first post looks just fine ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/test/",
+        "teaser":null}]
